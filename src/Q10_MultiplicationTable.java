a2fea6b324785a8380d11a47d6eb231ddebc6e05
shasap <sapan.shah.qa@gmail.com>
2022-05-01 13:55:39 +0100

    removed comment

--- src/Q10_MultiplicationTable.java
+++ src/Q10_MultiplicationTable.java
@@ -7,7 +7,6 @@ public class Q10_MultiplicationTable {
         Scanner multiplicationTable = new Scanner(System.in);
         System.out.println("Multiplication table of ");
 
-        // --- For user to select table, we have define integer a with scanner class.
 
         int a = multiplicationTable.nextInt();
 
